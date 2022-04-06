7faf09f42dcbc4203e242c97d7567609e47220c7
niart120 <38847256+niart120@users.noreply.github.com>
2022-04-07 02:10:35 +0900

    Create PlayerLinearSearch.cs

---
+++ PokemonBDSPRNGLibrary/RestoreSeed/PlayerLinearSearch.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using PokemonPRNG.XorShift128;
+
+namespace PokemonBDSPRNGLibrary.RestoreSeed
+{
+    public class PlayerLinearSearch
+    {
+        private readonly List<int> intervals = new List<int>();
+
+        public void AddInterval(int interval)
+        {
+            intervals.Add(interval);
+        }
+        public IEnumerable<(uint index, (uint s0, uint s1, uint s2, uint s3) state)> Search((uint s0, uint s1, uint s2, uint s3) state, uint max)
+        {
+            var blinkCache = new int[256];
+            var lastBlink = -1;
+            var idx = 0;
+            for(int i = 0; i < intervals.Count; idx++;)
+            {
+                if (state.BlinkPlayer() != PlayerBlink.None)
+                {
+                    if (lastBlink != -1)
+                    {
+                        var b = idx - lastBlink;
+                        blinkCache[i] = b;
+                    }
+                    lastBlink = idx;
+                }
+            }
+            bool check(int k)
+            {
+                for (int i = 0; i < intervals.Count; i++)
+                {
+                    var b = cache[(k + i) & 0xFF];
+                    if (intervals[i] != b) return false;
+                }
+
+                return true;
+            }
+            int head = 0, tail = intervals.Count;
+            while (head <= max)
+            {
+                while (state.BlinkPlayer() != PlayerBlink.None) idx++;
+                var b = idx - lastBlink;
+                blinkCache[tail++ & 0xFF] = b;
+                lastBlink = idx;
+                if (check(head++)) yield return ((uint)tail, state);
+            }
+        }
+    }
+}
